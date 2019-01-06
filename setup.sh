2f9002e5c91fae5432534d00b4ac97b6ea027545
Michael Lanthier <m2lanthier@gmail.com>
2019-01-05 17:12:25 -0800

    Basic github housekeeping

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Setup File
+#Run this setup script to set up environment variables as many tests/scripts
+#depend upon them.
+#
+#Easiest thing to do is to throw it in your .bashrc or .bash_profile and run it 
+#at startup.
+
 #Sets environment variable STUDYSLUGDIR as the absolute path to StudySlug's
 #directory.
 STUDYSLUGDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
